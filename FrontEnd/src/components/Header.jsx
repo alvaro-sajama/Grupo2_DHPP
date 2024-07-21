e21678a32aa01345c39890301966a102f79520f8
Roxana Benicio <roxanayesicavictoriabenicio@gmail.com>
2024-07-21 20:27:17 -0300

    login flotante

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -10,9 +10,11 @@ export function Header() {
     setDarkMode(!darkMode);
     if (!darkMode) {
       document.documentElement.classList.add('dark');
+      
     } else {
       document.documentElement.classList.remove('dark');
     }
+
   };
 
   const [display, setDisplay] = useState('hidden')
@@ -33,8 +35,80 @@ export function Header() {
         <button onClick={toggledisplay}>display</button>
         <div className={ `absolute ${display} place-items-center inset-0 bg-black bg-opacity-50`}>
           <div className="z-10 bg-white opacity-100">
-            <div onClick={toggledisplay}>X</div>
-            login
+            <div onClick={toggledisplay}>X</div>  
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                  <img
+                    alt="Your Company"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    className="mx-auto h-10 w-auto"
+                  />
+                  <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    Iniciar sesión
+                  </h2>
+                </div>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                  <form action="#" method="POST" className="space-y-6">
+                    <div>
+                      <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                        Email 
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          id="email"
+                          name="email"
+                          type="email"
+                          required
+                          autoComplete="email"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+                    </div>
+
+                    <div>
+                      <div className="flex items-center justify-between">
+                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                          Contraseña
+                        </label>
+                        <div className="text-sm">
+                          <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                          ¿Olvidado Su Contraseña?
+                          </a>
+                        </div>
+                      </div>
+                      <div className="mt-2">
+                        <input
+                          id="password"
+                          name="password"
+                          type="password"
+                          required
+                          autoComplete="current-password"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+                    </div>
+
+                    <div>
+                      <button
+                        type="submit"
+                        className="flex w-full justify-center rounded-md bg-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      >
+                        Login
+                      </button>
+                    </div>
+                  </form>
+
+                  <p className="mt-10 text-center text-sm text-gray-500">
+                    ¿No tiene cuenta?{' '}
+                    <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                      Registrarse
+                    </a>
+                  </p>
+                </div>
+              </div>
+
+            
           </div>
         </div>
 
